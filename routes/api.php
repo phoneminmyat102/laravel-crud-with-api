2bd2f3543b751c3a4b5c5c2b16b0a0d22c12f7fc
phoneminmyat102 <phoneminmyat102115@gmail.com>
2023-11-13 09:46:37 +0630

    modify

--- routes/api.php
+++ routes/api.php
@@ -6,24 +6,12 @@ use App\Http\Controllers\DummyApiController;
 use App\Http\Controllers\DeviceController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\MemberController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::group(['middleware' => 'auth:sanctum'], function(){
-    //All secure URL's
-        Route::apiResource('member', MemberController::class);
 
         Route::get('list/{id?}', [DeviceController::class, 'list']);
 
@@ -32,9 +20,4 @@ Route::group(['middleware' => 'auth:sanctum'], function(){
         Route::delete('delete/{id}', [DeviceController::class, 'delete']);
         Route::get('search/{name}', [DeviceController::class, 'search']);
     });
-
-
-
-
-
 Route::post('/login',[UserController::class,'index']);
